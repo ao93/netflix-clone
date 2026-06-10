144642a0026a5acdffad3ce5204347832121b9c7
Adolfo Ovalles <aovalles1357@gmail.com>
2026-06-10 14:10:29 -0400

    fix: use Bearer token auth for TMDB API

--- src/store/slices/apiSlice.ts
+++ src/store/slices/apiSlice.ts
@@ -1,8 +1,14 @@
-import { API_ENDPOINT_URL } from "src/constant";
+import { API_ENDPOINT_URL, TMDB_V3_API_KEY } from "src/constant";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
-  baseQuery: fetchBaseQuery({ baseUrl: API_ENDPOINT_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_ENDPOINT_URL,
+    prepareHeaders: (headers) => {
+      headers.set("Authorization", `Bearer ${TMDB_V3_API_KEY}`);
+      return headers;
+    },
+  }),
   endpoints: (build) => ({}),
 });
